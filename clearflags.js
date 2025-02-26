afbe17cf6a64955f411a227e92c37550c4d57fd3
Weslley Rocha <weslleytato.12@gmail.com>
2025-02-26 13:51:29 -0300

    clear flags from database

---
+++ clearflags.js
@@ -0,0 +1,46 @@
+import { extractPack, compilePack } from "@foundryvtt/foundryvtt-cli";
+import { readdirSync, readFileSync, writeFileSync } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log(__dirname, __filename);
+
+const packs = [
+  "classes",
+  "classes-e-subclasses-features",
+  "magias",
+  "racas",
+  "subclasses",
+];
+
+const clearFlags = (filePath) => {
+  const data = JSON.parse(readFileSync(filePath, "utf8"));
+
+  if (data.flags.plutonium) {
+    delete data.flags.plutonium;
+  }
+
+  writeFileSync(filePath, JSON.stringify(data, null, 2));
+};
+
+const clearFlagsInFolder = (folderPath) => {
+  const files = readdirSync(folderPath);
+  files.forEach((file) => {
+    clearFlags(join(folderPath, file));
+  });
+};
+
+const execute = async () => {
+  for (const pack of packs) {
+    const packPath = join(__dirname, "packs", pack);
+    const destPath = join(__dirname, "packs", pack, "_source");
+    await extractPack(packPath, destPath);
+    clearFlagsInFolder(destPath);
+    await compilePack(destPath, packPath);
+  }
+};
+
+execute();
