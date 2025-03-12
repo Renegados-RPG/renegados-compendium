1f53772ca64a52aec549e21cc756376739046152
Weslley Rocha <weslleytato.12@gmail.com>
2025-03-12 00:36:57 -0300

    fix broken links

--- commands/findBrokenLinks.js
+++ commands/findBrokenLinks.js
@@ -28,29 +28,36 @@ const mountCompendiumsMap = () => {
   spinner.succeed("Mounted compendiums map");
 };
 
+const MODULEPACKS = ["classes", "subclasses"];
 export const execute = async () => {
   mountCompendiumsMap();
-  // for (const pack of PACKS) {
-  const packPath = PACK_SOURCE_PATH("classes");
-  const files = readdirSync(packPath);
-  for (const file of files.map((f) => f).filter((f) => f.endsWith(".json"))) {
-    // console.log("Reading file", file);
-    const data = JSON.parse(readFileSync(join(packPath, file), "utf8"));
-    const grants = data.system.advancement.filter((a) => a.type === "ItemGrant");
-    grants.forEach((grant) => {
-      const items = grant.configuration.items;
-      items.forEach((item) => {
-        const regex = /compendium\.renegados-compendium\.(.*)\.Item\.(.*)/gi;
-        const match = regex.exec(item.uuid);
-        if (!match) {
-          console.log("No match", item.uuid, "in", data.name);
-          return;
-        }
-        if (!compendiunsMap[match[1]][match[2]]) {
-          console.log("Compendium not found", match[2], "in", data.name, "level", grant.level);
-          return;
-        }
-      });
-    });
+  for (const pack of MODULEPACKS) {
+    const packPath = PACK_SOURCE_PATH(pack);
+    const files = readdirSync(packPath);
+    for (const file of files.map((f) => f).filter((f) => f.endsWith(".json"))) {
+      try {
+        const data = JSON.parse(readFileSync(join(packPath, file), "utf8"));
+        if (data._key.startsWith("!folders!")) continue;
+        const grants = data.system.advancement.filter((a) => a.type === "ItemGrant");
+        grants.forEach((grant) => {
+          const items = grant.configuration.items;
+          items.forEach((item) => {
+            const regex = /compendium\.renegados-compendium\.(.*)\.Item\.(.*)/gi;
+            const match = regex.exec(item.uuid);
+            if (!match) {
+              console.log("No match", item.uuid, "in", data.name);
+              return;
+            }
+            if (!compendiunsMap[match[1]][match[2]]) {
+              console.log("Compendium not found", match[2], "in", data.name, "level", grant.level);
+              return;
+            }
+          });
+        });
+      } catch (err) {
+        console.log("Error reading file", file);
+        console.log(data);
+      }
+    }
   }
 };
